b5bbabad56a32683d9b8dac90cbb6daddd29abf0
MarcosNBJ <marcosnery.comp@gmail.com>
2021-01-02 23:24:44 -0300

    Setting up project with create-react-app

---
+++ 4chan-media-gallery/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+
+        App setup working !
+    </div>
+  );
+}
+
+export default App;
