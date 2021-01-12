787f3636f71b8178d47179d846669f21b0cca329
MarcosNBJ <marcosnery.comp@gmail.com>
2021-01-12 05:43:11 -0300

    Creating router and component structure

--- 4chan-media-gallery/src/App.js
+++ 4chan-media-gallery/src/App.js
@@ -5,8 +5,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+            <Router>
+            <Switch>
+            <Route exact path="/" component={MediaContainer} /> 
 
-        App setup working !
+            </Switch>
+        </Router>
+      
     </div>
   );
 }
