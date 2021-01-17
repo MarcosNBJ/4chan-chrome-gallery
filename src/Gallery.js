51c454e82c1f3a5c7d4eecc3bac84aff309c6a45
MarcosNBJ <marcosnery.comp@gmail.com>
2021-01-17 00:41:42 -0300

    refact: Refactoring gallery component

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./Gallery.css";
-import MediaContainer from './MediaContainer'; 
+import ScrollContainer from "react-indiana-drag-scroll";
+import "./MediaContainer.css";
 
 const media = require('4chan-get-media');
 
 function Gallery({thread_url}) {
 
     const [posts, setPosts] = useState([]);
-    const [mediaUrl, setmediaUrl] = useState("");
+    const [mediaUrl, setMediaUrl] = useState("");
 
     useEffect(() => {
         async function fetchData() {
@@ -19,12 +20,27 @@ function Gallery({thread_url}) {
 
       const viewMedia = (index) => {
         console.log("adasdad"); 
-        setmediaUrl(index);
+        setMediaUrl(index);
       };
 
     return (
       <div className="container">
-        {mediaUrl !== "" && <MediaContainer  posts={posts}/>}
+        {mediaUrl !== "" && 
+          <div>
+            <img className="mediaContainer" src={posts[mediaUrl].url} alt="" srcset=""/>
+            <div className="row">
+            <ScrollContainer className="row__posters">
+                {posts.map((post, index) => (
+                        <img className="row__poster" src={post.thumbnail} alt=""
+                        onClick={() =>
+                        viewMedia(index)
+                        }
+                        />
+                ))}
+            </ScrollContainer>
+            </div>
+          </div>
+        }
         {mediaUrl === "" && posts.map((post, index) => (
         <div className="responsive">
           <div className="gallery">
