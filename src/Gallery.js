9afae949d556840d8129900115e4214375b61431
MarcosNBJ <marcosnery.comp@gmail.com>
2021-01-16 23:23:15 -0300

    refact: splitting gallery in two components

---
+++ src/Gallery.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import "./Gallery.css";
+import MediaContainer from './MediaContainer'; 
+
+const media = require('4chan-get-media');
+
+function Gallery({thread_url}) {
+
+    const [posts, setPosts] = useState([]);
+    const [mediaUrl, setmediaUrl] = useState("");
+
+    useEffect(() => {
+        async function fetchData() {
+          const result = await media.get_thread_media(thread_url);
+          setPosts(result);
+        }
+        fetchData();
+      }, [thread_url]);
+
+      const viewMedia = (index) => {
+        console.log("adasdad"); 
+        setmediaUrl(index);
+      };
+
+    return (
+      <div className="container">
+        {mediaUrl !== "" && <MediaContainer  url={posts[mediaUrl].url}/>}
+        {mediaUrl === "" && posts.map((post, index) => (
+        <div className="responsive">
+          <div className="gallery">
+            <img className="card" src={post.thumbnail} alt=""
+             onClick={() =>
+              viewMedia(index)
+            }
+            />
+          </div>
+        </div>
+        ))}
+        <div className="clearfix"></div>
+     </div>
+    )
+}
+
+export default Gallery
--- src/MediaContainer.js
+++ src/MediaContainer.js
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
-import "./MediaContainer.css";
+import React from 'react'
 
-const media = require('4chan-get-media');
-
-function MediaContainer({thread_url}) {
-
-    const [posts, setPosts] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-          const result = await media.get_thread_media(thread_url);
-          setPosts(result);
-        }
-        fetchData();
-      }, [thread_url]);
-
-    console.log(posts)
+function MediaContainer({url}) {
 
     return (
-      <div className="container">
-        {posts.map((post) => (
-        <div className="responsive">
-          <div className="gallery">
-          <a href={post.url}>
-
-            <img className="card" src={post.thumbnail} alt=""/>
-            </a>
-          </div>
+        <div>
+            <img className="mediaContainer" src={url} alt=""/>
         </div>
-        ))}
-        <div className="clearfix"></div>
-     </div>
     )
 }
 
